a518925cb9ede22ba1292547f9444be07b32346d
KhanhPr1 <98386055+KhanhPr1@users.noreply.github.com>
2025-05-30 16:45:53 +0700

    Add files via upload

---
+++ Lab2/DemoCalculator/src/main/java/khanhnv/example/Calculator.java
@@ -0,0 +1,15 @@
+package khanhnv.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by 0");
+        return a / b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
